bb078944f0a2a7d0144fbb628bc162976accc7ff
Mishaa <mishapro@mm.st>
2020-07-12 15:38:06 +0200

    feat: add tailwindcss framework, google font and font awesome
    Tailwindcss can be now used in order to make good design
    
    Signed-off-by: Mishaa <mishapro@mm.st>

--- src/main.js
+++ src/main.js
@@ -5,11 +5,18 @@
  */
 
 import Vue from "vue";
+import Vuesax from "vuesax";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "@/assets/css/tailwindcss.scss";
+import "@/assets/css/main.scss";
+import "vuesax/dist/vuesax.css";
+
 Vue.config.productionTip = false;
+
+Vue.use(Vuesax, {});
 
 new Vue({
   router,
